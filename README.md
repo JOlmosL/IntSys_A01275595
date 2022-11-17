942a7672c8f78a9e31f5cbc9859fb2e3a5372a99
JesusOlmosLarios <69598051+JesusOlmosLarios@users.noreply.github.com>
2022-11-17 11:58:36 -0600

    Ready

--- README.md
+++ README.md
@@ -66,11 +66,11 @@ These functions are complementary for the agent and will make its life more easy
 
 3. get_units_by_type returns a list with instances of the input unit or units.
 
-![U_Type](./Images/Get_units_type.png)
+![Get_U](./Images/Get_units_type.png)
 
 4. can_do is a very important function, because it will check if the input action is available or not.
 
-![U_Type](./Images/Can_do.png)
+![Can_do](./Images/Can_do.png)
 
 ### 6. Completing Step
 As its name inquires, we´ll be completing our Step function
@@ -80,11 +80,20 @@ Now that we know were we are, it's easy to know where the enemy is, so we just s
 
 Resource gathering and knowledge of how much of them we have is important for creating almost everything, so we simply define that variables to obtain a counter of minerals and vespene's.
 
-![Imports](./Images/Step_1.png)
+![Steps](./Images/Step_1.png)
 
-#### Pylons
+### Probes
+> Used to gather resources and Warp Protoss structures.
 
-![Imports](./Images/Pylon.jpg)
+![Probe](./Images/Probe.jpg)
+
+I would consider Probes to be the most important unit in the game, withouth them we cant do anything. And in order for us to do anything, we need to verify that they are selected.
+
+![Probe_code](./Images/Probes_code.png)
+
+### Pylons
+
+![Pylon](./Images/Pylon.jpg)
 
 Because we are playing as Protoss, we'll need to use Pylons.
 The game describes Pylons as: 
@@ -92,76 +101,77 @@ The game describes Pylons as:
 
 Basically, Pylons are power outlets, indispensable for more powerfull structures.
 
-![Imports](./Images/Pylon_code.png)
+![Pylon_code](./Images/Pylon_code.png)
 
 I like the number 5, so until we have 5 built Pylons and have more than 100 minerals (Which is the price of building a Pylon), we'll check if a Prove is selected in order for it to being able to Warp Pylons. If we have it selected, our random function comes in handy to produce possible values in which the Pylon will be warped. 
 
 
-#### Gateways and Assimilators
+### Gateways and Assimilators
 Gateways are described in game as:
 > Warps in Protoss ground units
 
-![Imports](./Images/Gateway.jpg)
+![Gateway](./Images/Gateway.jpg)
 
 While Assimilators are described as:
 > Protoss structure used for harvesting Vespene Gas
 
-![Imports](./Images/Assimilator.jpg)
+![Assimilator](./Images/Assimilator.jpg)
 
-![Imports](./Images/G_A.png)
+![G_A_code](./Images/G_A.png)
 
 gates variable will return a list of Gateway objects. Then, we'll check if we have 2 gateways built and ≥ 150 minerals (Cost of the Gateway). Once we have resources, We check if a Probe is selected in order to Warp the Gateway in a random location.
 
 Nothing changes substancially for warping an Assimilator other than the cost of the minerals for warping it (Should be 75 at least).
 
-#### Cybernetics Core
+### Cybernetics Core
 
-![Imports](./Images/Cy_core.jpg)
+![Cybernetics](./Images/Cy_core.jpg)
 
 In order to use more than just Zealots for attacking the enemy player, we need to build at least one Cybernetics Core.
 The steps are the same as all of the avobe, except the cost to build the Cybernetics Core, wich sould be at least 150 minerals worth.
 
-![Imports](./Images/Cy_code.png)
+![Cybernetics_code](./Images/Cy_code.png)
 
-#### Zealots and Sentrys
+### Zealots and Sentrys
 Now that we have all of our Structures ready, we can start training Zealots and Sentrys.
 
 In game description of Zealots include but its not limitted to:
 > Can attack ground units
 
-![Imports](./Images/Zealot.jpg)
+![Zealot](./Images/Zealot.jpg)
 
 In the other hand, Sentrys are described as:
 > Ergy manipulator units. Can use ForceField, Guardian Shield and Hallucination.
 
-![Imports](./Images/Sentry.jpg)
+![Sentry](./Images/Sentry.jpg)
 
 Pretty cool, right?
 
-![Imports](./Images/Z_S_code.png)
+![Z_S_code](./Images/Z_S_code.png)
 
 We first check for our 2 gateways and that we at least have 100 minerals, we then procede to verify that a gateway is indeed selected, if thats not the case, we select a random gateway. We should limit our zealots to a number that we want, in this case i liked 6 so each gateway will only Warp 6 Zealots.
 
-Same story with the Sentrys but this time, both our Minerals and Vespene will be taken in consideration. Sentrys are cool but we might not need so many of them, so I choose to limit Sentrys to only 1 per Gateway.
+Same story with the Sentrys but this time, both our Minerals and Vespene will be taken in consideration. Sentrys are cool but we might not need so many of them, so I choose to limit Sentrys to only 2 per Gateway.
 
-#### Probes
-> Used to gather resources and Warp Protoss structures.
-
-![Imports](./Images/Probe.jpg)
-
-I would consider Probes to be the most important unit in the game, withouth them we cant do anything. And in order for us to do anything, we need to verify that they are selected.
-
-![Imports](./Images/Probes_code.png)
-
-#### Winning
+### Winning
 Now that we have everything ready, its time to take the ofensive.
 
-![Imports](./Images/Winning.png)
+![Winning](./Images/Winning.png)
 
 To start, we need to make sure we have at least 6 Zealot units trained. If thats the case, we select them and make sure that we can actually send them to attack the enemy.
 
 Story repeats itself with the Sentry, but in this case we only check for 1
 
+### Diagram
+If by any chance, you didnt understood anything that was mentioned above, dont worry, I got you covered.
+
+This Diagram covers how the code works in a simplified way, no need to thank me.
+
+![Diagram](./Images/Diagram.png)
+
+
 ## References
+-  Aboytes, J. P. (2020, November 6). Pysc2 protoss sentry and zealot agent. Medium. https://a01701249.medium.com/pysc2-protoss-sentry-and-zealot-agent-87e0f0f1388f 
 - Bansall, S. (2022, August 22). geeksforgeeks.org. https://www.geeksforgeeks.org/agents-artificial-intelligence/
 - Valdés B. (2019, August 19). Pysc2 install process. YouTube. https://www.youtube.com/watch?v=KYCxZBghVZ8
+- PySC2 - StarCraft II Learning Environment. (2022, November 17). GitHub. https://github.com/deepmind/pysc2
--- Smart_Protoss.py
+++ Smart_Protoss.py
@@ -5,15 +5,14 @@ from pysc2.lib import actions, features, units
 from absl import app
 import random
 
-
 class Protoss_Agent(base_agent.BaseAgent):
 
-    def __init__(self):
+    def __init__(self):                                     #Initialize a variable
         super(Protoss_Agent, self).__init__()
         self.first_pylon = None
         self.attack_coordinates = None
 
-    def unit_type_is_selected(self, obs, unit_type):
+    def unit_type_is_selected(self, obs, unit_type):        #simply sintax of unit type selected check
         if (len(obs.observation.single_select) > 0 and obs.observation.single_select[0].unit_type == unit_type):
             return True
 
@@ -22,16 +21,16 @@ class Protoss_Agent(base_agent.BaseAgent):
 
         return False
 
-    def get_units_by_type(self, obs, unit_type):
+    def get_units_by_type(self, obs, unit_type):            #simply sintax of unit selection by type
         return [unit for unit in obs.observation.feature_units if unit.unit_type == unit_type]
 
-    def can_do(self, obs, action):
+    def can_do(self, obs, action):                          #simply sintax of available actions check
         return action in obs.observation.available_actions
 
     def step(self, obs):
         super(Protoss_Agent, self).step(obs)
 
-        if obs.first():
+        if obs.first():                                     #Observes environment and determines enemy locations
             player_y, player_x = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.SELF).nonzero()
             xmean = player_x.mean()
             ymean = player_y.mean()
@@ -46,7 +45,7 @@ class Protoss_Agent(base_agent.BaseAgent):
         minerals = obs.observation.player.minerals
         vespene = obs.observation.player.vespene
         
-        #Attack
+        #Attacking
         zealots = self.get_units_by_type(obs, units.Protoss.Zealot)
         sentry = self.get_units_by_type(obs, units.Protoss.Sentry)
         if len(zealots) >= 6:
@@ -57,8 +56,7 @@ class Protoss_Agent(base_agent.BaseAgent):
             if self.can_do(obs, actions.FUNCTIONS.select_army.id):
                 return actions.FUNCTIONS.select_army("select")
         
-        
-        if len(sentry) >= 1:
+        if len(sentry) >= 2:
             if self.unit_type_is_selected(obs, units.Protoss.Sentry):
                 if self.can_do(obs, actions.FUNCTIONS.Attack_minimap.id):
                     return actions.FUNCTIONS.Attack_minimap("now", self.attack_coordinates)
@@ -66,8 +64,7 @@ class Protoss_Agent(base_agent.BaseAgent):
             if self.can_do(obs, actions.FUNCTIONS.select_army.id):
                 return actions.FUNCTIONS.select_army("select")
 
-
-        #Pylon
+        #Cybernetics Core Creation
         gates = self.get_units_by_type(obs, units.Protoss.Gateway)
 
         cybernetic = self.get_units_by_type(obs, units.Protoss.CyberneticsCore)
@@ -77,7 +74,8 @@ class Protoss_Agent(base_agent.BaseAgent):
                     x = random.randint(0, 83)
                     y = random.randint(0, 83)
                     return actions.FUNCTIONS.Build_CyberneticsCore_screen("now", (x, y))
-          
+
+        #Pylons Creation
         pylons = self.get_units_by_type(obs, units.Protoss.Pylon)
         if len(pylons) < 5 and minerals >= 100:
             if self.unit_type_is_selected(obs, units.Protoss.Probe):
@@ -86,14 +84,15 @@ class Protoss_Agent(base_agent.BaseAgent):
                     y = random.randint(0, 83)
                     return actions.FUNCTIONS.Build_Pylon_screen("now", (x, y))
 
-        #Gateways
+        #Gateways Creation
         if len(gates) < 2 and minerals >= 150:
             if self.unit_type_is_selected(obs, units.Protoss.Probe):
                 if self.can_do(obs, actions.FUNCTIONS.Build_Gateway_screen.id):
                     x = random.randint(0, 83)
                     y = random.randint(0, 83)
                     return actions.FUNCTIONS.Build_Gateway_screen("now", (x, y))
-
+        
+        #Assimilators Creation
         gas = self.get_units_by_type(obs, units.Protoss.Assimilator)
         if len(gates) == 2 and minerals >= 75 and len(gas) == 0:
             if self.unit_type_is_selected(obs, units.Protoss.Probe):
@@ -102,7 +101,7 @@ class Protoss_Agent(base_agent.BaseAgent):
                     y = random.randint(0, 83)
                 return actions.FUNCTIONS.Build_Assimilator_screen("now", (x, y))
         
-        #Units                   
+        #Train Zealots and Sentrys                   
         if len(gates) == 2 and minerals >= 100:
             if self.unit_type_is_selected(obs, units.Protoss.Gateway):
                 zealots = self.get_units_by_type(obs, units.Protoss.Zealot)
@@ -116,7 +115,7 @@ class Protoss_Agent(base_agent.BaseAgent):
         if len(cybernetic) == 1 and vespene >= 100 and minerals >= 50:
             if self.unit_type_is_selected(obs, units.Protoss.Gateway):
                 sentry = self.get_units_by_type(obs, units.Protoss.Sentry)
-                if len(sentry) <= 1:
+                if len(sentry) <= 2:
                     if self.can_do(obs, actions.FUNCTIONS.Train_Sentry_quick.id):
                         return actions.FUNCTIONS.Train_Sentry_quick("now")
 
@@ -124,10 +123,7 @@ class Protoss_Agent(base_agent.BaseAgent):
             return actions.FUNCTIONS.select_point("select_all_type", (z.x, z.y))
             
         #Select Probe units 
-
-        probes = self.get_units_by_type(obs, units.Protoss.Probe)
-        print(len(probes))
-        
+        probes = self.get_units_by_type(obs, units.Protoss.Probe)        
         if len(probes) > 0:
             probe = random.choice(probes)
             return actions.FUNCTIONS.select_point("select_all_type", (probe.x, probe.y))
@@ -142,12 +138,12 @@ def main(unused):
             with sc2_env.SC2Env(
                 map_name ="Simple64",                                                       #Game map
                 players=[sc2_env.Agent(sc2_env.Race.protoss),                               #Agent race
-                         sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.easy)],   #Difficulty
+                         sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.easy)],        #Difficulty
                 agent_interface_format = features.AgentInterfaceFormat(
                     feature_dimensions = features.Dimensions(screen=84, minimap=64),        #Screen resolution
                     use_feature_units = True
                 ),
-                step_mul = 16,                                                               #Game Speed
+                step_mul = 16,                                                              #Game Speed
                 game_steps_per_episode = 0,                                                 #Game length
                 visualize=True
             ) as env:
@@ -169,5 +165,3 @@ def main(unused):
 
 if __name__ == "__main__":
     app.run(main)
-
-    #https://a01701249.medium.com/pysc2-protoss-sentry-and-zealot-agent-87e0f0f1388f
